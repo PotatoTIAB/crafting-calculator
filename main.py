7a29c277582625241e6040c99dd130db593ee431
PotatoTIAB <serkanicoz2005@gmail.com>
2022-08-17 16:08:45 +0300

    WIP: adding recipe calculation.

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("hello world!")
+class CraftingCalculator:
+    def __init__(self):
+        self.recipes = {
+            "foo": [["bar"]]
+        }
+        self.items = ["foo", "bar"]
+
+    def calculate(self, item):
+        if item in self.recipes.keys():
+            return self.calculate(self.recipes.get(item)[0])
+        else:
+            return item
+
+calc = CraftingCalculator()
+print(calc.calculate("foo"))
