a15578aaa2f6410cba172db510693b449ee9c1b7
PotatoTIAB <serkanicoz2005@gmail.com>
2022-08-17 16:39:16 +0300

    WIP: adding recipe calculation.

--- main.py
+++ main.py
@@ -5,11 +5,18 @@ class CraftingCalculator:
         }
         self.items = ["foo", "bar"]
 
-    def calculate(self, item):
-        if item in self.recipes.keys():
-            return self.calculate(self.recipes.get(item)[0])
-        else:
-            return item
+    def recipe(self, arr):
+        _result = []
+        if not isinstance(arr, list):
+            arr = [arr]
+        for item in arr:
+            if item in self.recipes.keys():
+                return self.calculate(self.recipes.get(item)[0])
+            else:
+                return item
+    
+    def recc(self, arr):
+        pass
 
 calc = CraftingCalculator()
 print(calc.calculate("foo"))
