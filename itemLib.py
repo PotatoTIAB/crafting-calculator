9ec6df67154c2eb86ba9f2fa24a88c3d66e64706
PotatoTIAB <serkanicoz2005@gmail.com>
2022-08-17 17:46:57 +0300

    now items have ids and count that can be added.

--- itemLib.py
+++ itemLib.py
@@ -1,5 +1,20 @@
 class ItemStack:
-    pass
+    def __init__(self, ID="air", count=1):
+        self.id = ID
+        self.count = count
+    
+    def __add__(self, x):
+        if isinstance(x, self.__class__):
+            if self.id == x.id:
+                return self.__class__(self.id, self.count + x.count)
+            else:
+                raise NotImplementedError("You can't add items of 2 different kinds yet.")
+        else:
+            raise TypeError("Invalid summation.")
+
 
 class ItemContainer:
+    pass
+
+if __name__ == "__main__":
     pass
