285aca29a0e2f53f4c8658a2f1c81e4cb3690347
PotatoTIAB <serkanicoz2005@gmail.com>
2022-08-25 12:23:07 +0300

    new calculate function.

--- main.py
+++ main.py
@@ -7,14 +7,27 @@ class CraftingCalculator:
             ItemContainer(ItemStack("bar")): ItemContainer(ItemStack("foobar")),
         }
 
-    def calculate(self, arr):
-        _result = []
-        if not isinstance(arr, list):
-            arr = [arr]
-        for item in self.substitute(arr):
-            _result.append(item)
-        return _result
-    
+
+    def calculate(self, cont):
+        if not isinstance(cont, ItemContainer):
+            _item, cont = cont, ItemContainer()
+            if isinstance(_item, str):
+                _item = ItemStack(_item)
+            elif isinstance(_item, list):
+                if len(_item) == 2:
+                    _item = ItemStack(_item[0], _item[1])
+                elif len(cont) == 1:
+                    _item = ItemStack(_item[0])
+                else:
+                    raise ValueError(f"Expected a list with 1 or 2 elements, got {len(cont)} instead.")
+            cont.add(_item)
+        
+        # for item in self.substitute(cont):
+        #     _result.append(item)
+        
+        return cont
+
+
     def substitute(self, arr):
         for item in arr:
             if item in self.recipes.keys():
@@ -25,4 +38,4 @@ class CraftingCalculator:
 
 
 calc = CraftingCalculator()
-print(calc.calculate("foobar"))
+print(calc.calculate("foo"))
